d9e36579edae8703575293cd8f3bda49c6f9a430
suriya4code <suriya4code@gmail.com>
2021-11-19 00:18:41 +0530

    Resolved cmd issue

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
 FROM python:3.7
+LABEL maintainer="Suriyaprakash suriya4code@gmail.com"
 WORKDIR /tesseract-flask
+RUN apt-get update -y
+# RUN apt-get install -y python3-pip python-dev build-essential
 ENV FLASK_APP=main.py
 ENV FLASK_ENV = production
 ENV FLASK_RUN_HOST=0.0.0.0
 # RUN apk add --no-cache gcc musl-dev linux-headers
 COPY requirements.txt requirements.txt
-RUN apt-get update && apt-get install -y python3-opencv
+RUN apt-get install -y python3-opencv
 RUN apt-get install -y tesseract-ocr
 RUN pip install opencv-python
 RUN pip install -r requirements.txt
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import io
 import cv2
 import numpy as np
 import pytesseract
+from PIL import Image
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,7 +14,7 @@ upload_parser = api.parser()
 upload_parser.add_argument('file',location='files',type=FileStorage)
 
 def read_image(img):
-    pytesseract.pytesseract.tesseract_cmd =     './.apt/usr/bin/tesseract'
+    # pytesseract.pytesseract.tesseract_cmd =  '../usr/bin/tesseract'
     content = pytesseract.image_to_string(img)
     return content
 
@@ -22,10 +23,7 @@ def read_image(img):
 @api.route('/predict/')
 @api.expect(upload_parser)
 class Tesseract(Resource):
-    def get(self):
-        print("reached get tesseract")
-        return "predicted"
-    
+        
     def post(self):
         print("file received")
         args = upload_parser.parse_args()
@@ -33,8 +31,10 @@ class Tesseract(Resource):
         image_stream = io.BytesIO(uploaded_file.read())
         image_stream.seek(0)
         filebytes = np.asarray(bytearray(image_stream.read()),dtype=np.uint8)
-        frame = cv2.imdecode(filebytes, cv2.IMREAD_COLOR)
-        label = read_image(frame)
+        img_cv = cv2.imdecode(filebytes, cv2.IMREAD_COLOR)
+        img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+        print(pytesseract.image_to_string(img_rgb))
+        label = read_image(img_rgb)
         return label
 
 if __name__ == "__main__":
